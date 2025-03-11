147020056b96e0976b4094492140174d95cabbd3
MMA21 <migelwot@gmail.com>
2025-03-11 20:56:22 +0300

    Добавил Footer

--- src/components/ui/Footer/Footer.jsx
+++ src/components/ui/Footer/Footer.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import {Stack, Typography} from "@mui/material";
 
-function Footer(props) {
+function Footer() {
     return (
-        <p>Footer</p>
+        <Stack component='footer' sx={{
+            paddingTop: 4,
+            paddingBottom: 4,
+            flexDirection: { sm: 'row' },
+            justifyContent: { sm: 'space-between' },
+            alignItems: { sm: 'center' },
+            marginTop: 'auto',
+        }}>
+            <Typography variant={'body2'}>
+                &copy; {new Date().getFullYear()} &laquo;cinema&raquo;<br/>
+                Данный сайт создан исключительно в обучающих целях. <br/>
+                Все права принадлежат правообладателям.
+            </Typography>
+            <Typography variant={'h4'} color={'primary'}>
+                cinema
+            </Typography>
+        </Stack>
     );
 }
 
