6d90c313c285d172c77707fadc592ffbb97b4fd3
MMA21 <migelwot@gmail.com>
2025-03-24 02:19:54 +0300

    Обновил store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import currentSlice from '../features/CurrentSlice.js'
+import {kinopoiskApi} from "../services/kinopoiskApi.js";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        [kinopoiskApi.reducerPath]: kinopoiskApi.reducer,
+        currentSlice: currentSlice,
+    },
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware().concat(kinopoiskApi.middleware)
 })
